5ca3206fe85690efa1f704d112b6e249bf0018d4
Léa <leaserrano1119@gmail.com>
2023-12-05 17:34:14 +0100

    Ajout de la partie métrique

--- build-superpixels-project-Qt_5_15_3_qt5-Debug/moc_mainwindow.cpp
+++ build-superpixels-project-Qt_5_15_3_qt5-Debug/moc_mainwindow.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[11];
-    char stringdata0[215];
+    QByteArrayData data[13];
+    char stringdata0[277];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -42,7 +42,9 @@ QT_MOC_LITERAL(6, 131, 5), // "index"
 QT_MOC_LITERAL(7, 137, 24), // "on_denoise_algo_selected"
 QT_MOC_LITERAL(8, 162, 33), // "on_submit_button_download_cli..."
 QT_MOC_LITERAL(9, 196, 9), // "saveImage"
-QT_MOC_LITERAL(10, 206, 8) // "filePath"
+QT_MOC_LITERAL(10, 206, 8), // "filePath"
+QT_MOC_LITERAL(11, 215, 29), // "on_submit_button_algo_clicked"
+QT_MOC_LITERAL(12, 245, 31) // "on_submit_button_metric_clicked"
 
     },
     "MainWindow\0on_submit_button_upload_clicked\0"
@@ -51,7 +53,8 @@ QT_MOC_LITERAL(10, 206, 8) // "filePath"
     "on_noise_algo_selected\0index\0"
     "on_denoise_algo_selected\0"
     "on_submit_button_download_clicked\0"
-    "saveImage\0filePath"
+    "saveImage\0filePath\0on_submit_button_algo_clicked\0"
+    "on_submit_button_metric_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -61,7 +64,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       7,   14, // methods
+       9,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -69,13 +72,15 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   49,    2, 0x0a /* Public */,
-       3,    0,   50,    2, 0x0a /* Public */,
-       4,    0,   51,    2, 0x0a /* Public */,
-       5,    1,   52,    2, 0x0a /* Public */,
-       7,    1,   55,    2, 0x0a /* Public */,
-       8,    0,   58,    2, 0x0a /* Public */,
-       9,    1,   59,    2, 0x0a /* Public */,
+       1,    0,   59,    2, 0x0a /* Public */,
+       3,    0,   60,    2, 0x0a /* Public */,
+       4,    0,   61,    2, 0x0a /* Public */,
+       5,    1,   62,    2, 0x0a /* Public */,
+       7,    1,   65,    2, 0x0a /* Public */,
+       8,    0,   68,    2, 0x0a /* Public */,
+       9,    1,   69,    2, 0x0a /* Public */,
+      11,    0,   72,    2, 0x0a /* Public */,
+      12,    0,   73,    2, 0x0a /* Public */,
 
  // slots: parameters
     QMetaType::Void,
@@ -85,6 +90,8 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void, QMetaType::Int,    6,
     QMetaType::Void,
     QMetaType::Void, QMetaType::QString,   10,
+    QMetaType::Void,
+    QMetaType::Void,
 
        0        // eod
 };
@@ -102,6 +109,8 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 4: _t->on_denoise_algo_selected((*reinterpret_cast< int(*)>(_a[1]))); break;
         case 5: _t->on_submit_button_download_clicked(); break;
         case 6: _t->saveImage((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 7: _t->on_submit_button_algo_clicked(); break;
+        case 8: _t->on_submit_button_metric_clicked(); break;
         default: ;
         }
     }
@@ -136,13 +145,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 7)
+        if (_id < 9)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 7;
+        _id -= 9;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 7)
+        if (_id < 9)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 7;
+        _id -= 9;
     }
     return _id;
 }
